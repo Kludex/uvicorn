1918937f8cddb42dcb49f21142f12ac5d39b2ca8
Marcelo Trylesinski <marcelotryle@gmail.com>
2026-07-31 18:12:06 +0100

    Separate flow-control wait from disconnect check

--- uvicorn/protocols/websockets/websockets_sansio_impl.py
+++ uvicorn/protocols/websockets/websockets_sansio_impl.py
@@ -394,6 +394,7 @@ class WebSocketsSansIOProtocol(asyncio.Protocol):
 
     async def send(self, message: ASGISendEvent) -> None:
         await self.writable.wait()
+
         if self.transport.is_closing():
             raise ClientDisconnected()
 
--- uvicorn/protocols/websockets/wsproto_impl.py
+++ uvicorn/protocols/websockets/wsproto_impl.py
@@ -353,6 +353,7 @@ class WSProtocol(asyncio.Protocol):
 
     async def send(self, message: ASGISendEvent) -> None:
         await self.writable.wait()
+
         if self.transport.is_closing():
             raise ClientDisconnected()
 
